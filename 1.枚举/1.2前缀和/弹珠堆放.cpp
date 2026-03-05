fc144099f84fcb37bb8c5a05e9fbe94e004b3e15
yangsheep123 <mianhuatangit@gmail.com>
2026-03-05 21:20:25 +0800

    first commit

--- 1.枚举/1.2前缀和/弹珠堆放.cpp
+++ 1.枚举/1.2前缀和/弹珠堆放.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main(){
-    
+    vector<int> pre(1000000);
+    pre[0]=0;
+    pre[1]=1,pre[2]=4,pre[3]=10,pre[4]=20;
+    vector<int> a(1000000);
+    a[0]=0,a[1]=1,a[2]=3,a[3]=6,a[4]=10;
+    int dis=5,i=5;
+    while(1){
+        a[i]=a[i-1]+dis;
+        dis++;
+        pre[i]=pre[i-1]+a[i];
+        if(pre[i]>=20230610) break;
+        i++;
+    }
+    cout<<i-1;
     return 0;
 }
